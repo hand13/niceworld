2013c6cbc7ab7c5a277302d67ed2640f12ae9a20
hand13 <1101738345@qq.com>
2017-03-27 19:54:13 +0800

    nice

---
+++ A.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+import java.awt.*;
+public class A extends JFrame{
+	public static void main(String[] args){
+		A f=new A();
+		f.setTitle("hello world");
+		f.setVisible(true);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
