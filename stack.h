6a9d6a29fc693079be5e53128404b01d6a102bab
hand13 <1101738345@qq.com>
2017-03-27 20:11:36 +0800

    few

---
+++ stack.h
@@ -0,0 +1,13 @@
+#ifndef STACK_H
+#define STACK_H
+template<class T>
+class stack{
+	public:
+		virtual ~stack(){}
+		virtual bool empty()const =0;
+		virtual int size()const =0;
+		virtual T &top()=0;
+		virtual void push(const T & theElement) =0;
+}
+
+
